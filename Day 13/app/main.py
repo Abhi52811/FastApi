172a9556a17bbe21de91ab2246d1af8d16056b7a
Abhishek Gupta <abhishekgupta52811@gmail.com>
2025-05-02 14:06:32 +0530

    Day 13 [UPDATING...]

--- Day 13/app/main.py
+++ Day 13/app/main.py
@@ -3,9 +3,24 @@ from .databases import engine
 from . import models
 from .routers import post,user,auth, vote
 
+from fastapi.middleware.cors import CORSMiddleware
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "https://www.google.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
